fe9158e0f440d1b6791601988f56fffb93437bc6
willfxtzsFork <willfxtzs@gmail.com>
2026-02-03 11:40:42 +0000

    files update

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "badminton-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
